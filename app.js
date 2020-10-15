751d3c2c21eeeb3e1e9f34875bad0f7e210adc52
Raaf <99051006@mydavinci.nl>
2020-10-15 13:28:09 +0200

    StartPage and button
    Created a starting page and created a button to hide starting page and show TrainerPage

---
+++ app.js
@@ -0,0 +1,12 @@
+const buttonStart = document.getElementById("buttonStart");
+const startPage = document.getElementById("startPageContainer");
+const trainerPage = document.getElementById("trainerPageContainer");
+
+buttonStart.onclick = function() {
+    startPage.style.display = "none";
+    trainerPage.style.display = "block";
+    
+}
+
+
+

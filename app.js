b962d6ddb70443e85f678f23957556d375918476
Raaf <99051006@mydavinci.nl>
2020-11-04 11:06:04 +0100

    Shuffle function added
    thanks to this source https://javascript.info/task/shuffle#:~:text=Write%20the%20function%20shuffle(array,%2C%202%5D%20%2F%2F%20...

--- app.js
+++ app.js
@@ -9,18 +9,26 @@ const people = [
     {name: 'Bill Cosby', img: "img/people/person3.jpg"},
     {name: 'Batman', img: "img/people/person4.jpg"}
 ]
-
+const names = ['Barack Obama','Donald Trump','Bill Cosby','Batman'];
+const photos = ["img/people/person1.jpg","img/people/person2.jpg","img/people/person3.jpg","img/people/person4.jpg"];
+    
 buttonStart.onclick = function() {
     startPage.style.display = "none";
     trainerPage.style.display = "block";
     createTable();
 }
+// shuffle
+function shuffle(array) {
+    array.sort(() => Math.random() - 0.5);
+  }
 
 function createTable() {
+    shuffle(photos);
+    shuffle(names);
     for (let index = 0; index < people.length; index++) {
         var row = trainerTable.insertRow(1);
         var cell1 = row.insertCell(0); 
-        cell1.innerHTML = people[index].name;
+        cell1.innerHTML = names[index];
         cell1.id = "name" + index; 
         cell1.addEventListener("click", function(){
             console.log(document.getElementById('name'+index).innerHTML);
@@ -31,7 +39,7 @@ function createTable() {
             console.log(document.getElementById('img'+index).innerHTML);
         });
         var imageCell2 = document.createElement("img");
-        imageCell2.src = people[index].img;
+        imageCell2.src = photos[index];
         imageCell2.classList.add("imagePerson");
         document.getElementById('img'+index).appendChild(imageCell2); 
 
